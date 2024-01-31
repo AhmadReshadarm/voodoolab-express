5903482d1eb0f3d287d020a564f784cd73bf852e
Rishad <armreshad@gmail.com>
2024-01-31 10:46:11 +0300

    intial commit

---
+++ core/entities/barcodes/barcodes.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+
+@Entity()
+export class Barcodes {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @IsNotEmpty()
+  @Column({ unique: true })
+  code: string;
+
+  @Column({ default: false })
+  checked: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(args?: { code: string; checked: boolean }) {
+    if (args) {
+      this.code = args.code;
+      this.checked = args.checked;
+    }
+  }
+}
